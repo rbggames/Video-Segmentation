e10c4875a50a803e89ccad9cdf798e716f82c7ab
Matthew Boyce <boycem97@hotmail.com>
2017-11-07 16:26:38 +0000

    Tracker testing

--- Video Segmentation/TrackerTest.cpp
+++ Video Segmentation/TrackerTest.cpp
@@ -10,7 +10,68 @@ using namespace std;
 #define SSTR( x ) static_cast< std::ostringstream & >( \
 ( std::ostringstream() << std::dec << x ) ).str()
 
-int main1(int argc, char **argv)
+const int maxObjects = 10;
+
+int find_contour(Mat image,cv::Rect2d* boundingBoxes, int maxBoundingBoxes)
+{
+	Mat src_mat, gray_mat, canny_mat;
+	Mat contour_mat;
+	Mat bounding_mat;
+	Mat out;
+
+	contour_mat = image.clone();
+	bounding_mat = image.clone();
+
+	cvtColor(image, gray_mat, CV_BGR2GRAY);
+
+	// apply canny edge detection
+
+	Canny(image, canny_mat, 30, 128, 3, false);
+
+	//3. Find & process the contours
+	//3.1 find contours on the edge image.
+
+	vector< vector< cv::Point> > contours;
+	findContours(canny_mat, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	//3.2 draw contours & property value on the source image.
+
+	int largest_area = 0;
+	int largest_contour_index = 0;
+	Rect bounding_rect;
+
+
+	image.copyTo(out);
+	for (size_t i = 0; i< contours.size(); i++) // iterate through each contour.
+	{
+		// draw rectangle around the contour:
+		cv::Rect boundingBox = boundingRect(contours[i]);
+		if (i < maxBoundingBoxes)
+			boundingBoxes[i] = boundingBox;
+		cv::rectangle(out, boundingBox, cv::Scalar(255, 0, 255)); // if you want read and "image" is color image, use cv::Scalar(0,0,255) instead
+
+																  // you aren't using the largest contour at all? no need to compute it...
+																  /*
+																  double area = contourArea(contours[i]);  //  Find the area of contour
+
+																  if (area > largest_area)
+																  {
+																  largest_area = area;
+																  largest_contour_index = i;               //Store the index of largest contour
+																  bounding_rect = boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+																  }
+																  */
+	}
+
+	//drawContours(image, contours, largest_contour_index, Scalar(0, 255, 0), 2);
+
+	imshow("Bounding ", out);
+
+	return contours.size();
+}
+
+
+int main(int argc, char **argv)
 {
 	Point rook_points[1][7];
 	int w = 60;
@@ -37,31 +98,32 @@ int main1(int argc, char **argv)
 	// Create a tracker
 	string trackerType = trackerTypes[2];
 
-	Ptr<Tracker> tracker;
-
-#if (CV_MINOR_VERSION < 3)
-	{
-		tracker = Tracker::create(trackerType);
+	Ptr<Tracker> trackers[maxObjects];
+	for (int i = 0; i < maxObjects; i++) {
+		#if (CV_MINOR_VERSION < 3)
+			{
+				tracker[i] = Tracker::create(trackerType);
+			}
+		#else
+			{
+				if (trackerType == "BOOSTING")
+					trackers[i] = TrackerBoosting::create();
+				if (trackerType == "MIL")
+					trackers[i] = TrackerMIL::create();
+				if (trackerType == "KCF")
+					trackers[i] = TrackerKCF::create();
+				if (trackerType == "TLD")
+					trackers[i] = TrackerTLD::create();
+				if (trackerType == "MEDIANFLOW")
+					trackers[i] = TrackerMedianFlow::create();
+				if (trackerType == "GOTURN")
+					trackers[i] = TrackerGOTURN::create();
+			}
+		#endif
 	}
-#else
-	{
-		if (trackerType == "BOOSTING")
-			tracker = TrackerBoosting::create();
-		if (trackerType == "MIL")
-			tracker = TrackerMIL::create();
-		if (trackerType == "KCF")
-			tracker = TrackerKCF::create();
-		if (trackerType == "TLD")
-			tracker = TrackerTLD::create();
-		if (trackerType == "MEDIANFLOW")
-			tracker = TrackerMedianFlow::create();
-		if (trackerType == "GOTURN")
-			tracker = TrackerGOTURN::create();
-	}
-#endif
 	// Read video
 	VideoCapture video(0);
-
+	
 	// Exit if video is not opened
 	if (!video.isOpened())
 	{
@@ -73,55 +135,90 @@ int main1(int argc, char **argv)
 	// Read first frame
 	Mat frame;
 	bool ok = video.read(frame);
-
+	GaussianBlur(frame, frame, Size(7, 7), 0, 0);
+	frame.setTo(Scalar(0, 0, 0));
 	fillPoly(frame, ppt, npt, 1,
 		Scalar(255, 0, 255),
 		8);
+	for (int i = 0; i < 7; i++) {
+		rook_points[0][i].x += 100;
+		rook_points[0][i].y += 300;
+	}
+	fillPoly(frame, ppt, npt, 1,
+		Scalar(255, 0, 0),
+		8);
+	for (int i = 0; i < 7; i++) {
+		rook_points[0][i].x -= 100;
+		rook_points[0][i].y -= 300;
+	}
 
+	cv::Rect2d objectBoundingBoxes[maxObjects];
+	const int numObjects = min(maxObjects, find_contour(frame, objectBoundingBoxes,maxObjects));
 	// Define initial boundibg box
-	Rect2d bbox(287, 23, 86, 320);
+	//Rect2d bbox(287, 23, 86, 320);
 
 	// Uncomment the line below to select a different bounding box
-	bbox = selectROI(frame, false);
+	//bbox = selectROI(frame, false);
 
 	// Display bounding box.
-	rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+	for (int i=0; i < numObjects; i++) {
+		rectangle(frame, objectBoundingBoxes[i], Scalar(255, 0, 0), 2, 1);
+		trackers[i]->init(frame, objectBoundingBoxes[i]);
+	}
 	imshow("Tracking", frame);
 
-	tracker->init(frame, bbox);
+	
 
 	while (video.read(frame))
 	{
+		frame.setTo(Scalar(0, 0, 0));
+		GaussianBlur(frame, frame, Size(7, 7), 0, 0);
 		for (int i = 0; i < 7; i++) {
 			rook_points[0][i].x += 1;
-			rook_points[0][i].y += 3;
+			rook_points[0][i].y += 1;
 		}
 		fillPoly(frame, ppt, npt, 1,
 			Scalar(255, 0, 255),
 			8);
-
+		for (int i = 0; i < 7; i++) {
+			rook_points[0][i].x += 100;
+			rook_points[0][i].y += 300;
+		}
+		fillPoly(frame, ppt, npt, 1,
+			Scalar(255, 0, 0),
+			8);
+		for (int i = 0; i < 7; i++) {
+			rook_points[0][i].x -= 100;
+			rook_points[0][i].y -= 300;
+		}
 
 		// Start timer
 		double timer = (double)getTickCount();
-
+		
 		// Update the tracking result
-		bool ok = tracker->update(frame, bbox);
+		bool* ok = (bool*) malloc(sizeof(bool)*numObjects);
+		for (int i = 0; i < numObjects; i++) {
+			trackers[i]->update(frame, objectBoundingBoxes[i]);
+			if (ok[i])
+			{
+				// Tracking success : Draw the tracked object
+				Mat object(frame.size(), CV_8UC3),mask(frame.size(), CV_8U, Scalar(0));
+				object.setTo(Scalar(0, 255, 0));
+				rectangle(mask, objectBoundingBoxes[i], Scalar(255), CV_FILLED, 1);
+				frame.copyTo(object, mask);
+				imshow("Tracked"+i, object);
+				rectangle(frame, objectBoundingBoxes[i], Scalar(255, 0, 0), 2, 1);
+			}
+			else
+			{
+				// Tracking failure detected.
+				putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
+			}
+		}
+		free(ok);
 
 		// Calculate Frames per second (FPS)
 		float fps = getTickFrequency() / ((double)getTickCount() - timer);
-
-
-		if (ok)
-		{
-			// Tracking success : Draw the tracked object
-			rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
-		}
-		else
-		{
-			// Tracking failure detected.
-			putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
-		}
-
 		// Display tracker type on frame
 		putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
 
